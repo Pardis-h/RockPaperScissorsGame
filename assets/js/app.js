636e7416d565283d3afef50576ae821ae26a8762
Pardis Haghdoust <pardis.haghdoust@gmail.com>
2022-06-14 16:02:06 +0430

    Html an Style Done

---
+++ assets/js/app.js
@@ -0,0 +1,6 @@
+document.querySelector('.rules-btn').addEventListener("click",e=>{
+    document.querySelector('.rules').style.display="block";
+})
+document.querySelector('.close').addEventListener("click",e=>{
+    document.querySelector('.rules').style.display="none";
+})
